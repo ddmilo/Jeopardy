ab96bb05a5283b24ebeae5e7346152ccdc782f8d
ddmilo <ddmilo1029@gmail.com>
2017-07-12 23:01:48 -0400

    generating boxes through length of array

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,30 +1,53 @@
 
 
-var questionsAnswers = [{
+var questions = [{
     question: 'What does html stand for?',
-    answer : 'Hyper Text Markup Language'
+    answer : 'Hyper Text Markup Language',
+    points: 500
+},
+    {
+    question: 'What does html stand for?',
+    answer : 'Hyper Text Markup Language',
+    points: 500
+    
 }];
 
 var playerScore = 0;
 
-
-function whenClicked(){
-    var userInput = prompt(questionsAnswers[0].question);
-    if (userInput === questionsAnswers[0].answer){
-        alert('correct');
-        playerScore += 100;
-        updateCashBet();
-    };
+for(i=0; i < questions.length; i++){
+    var tile = document.createElement('tiles');
+    tile.className = 'tiles';
+    tile.innerHTML = questions[i].points;
+    document.body.appendChild(tile);
 }
+
+
+
+
+
+
+// function whenClicked(){
+//     var userInput = prompt(questionsAnswers[0].question).toLowerCase;
+//     if (userInput === questionsAnswers[0].answer){
+//         alert('correct');
+//         playerScore += questionsAnswers[0].points;
+//         updateCashBet();
+//     }
+//     else {
+//         alert('wrong');
+//         playerScore -= questionsAnswers[0].points;
+//         updateCashBet();
+//     }
+// }
    
-function updateCashBet() {
-  $('#playerScore').text('Score:  ' + playerScore.toString());
-}
+// function updateCashBet() {
+//   $('#playerScore').text('Score:  ' + playerScore.toString());
+// }
 
 
-console.log(questionsAnswers[0].answer);
+// console.log(questionsAnswers[0].answer);
 
 
-$('.test').click(function(){
-    whenClicked();
-});
+// $('.test').click(function(){
+//     whenClicked();
+// });
