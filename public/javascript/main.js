4fd64eae1f9462473e093b4639a9b50be8b35fd0
ddmilo <ddmilo1029@gmail.com>
2017-07-15 19:29:37 -0400

    re-wrote most of the js into jquery

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -35,15 +35,28 @@ var questions = [{
 var playerScore = 0;
 
 for (i = 0; i < questions.length; i++) {
-  var tile = document.createElement('tiles');
-  tile.className = 'tiles';
-  tile.innerHTML = questions[i].points;
-  tile.setAttribute("question", questions[i].question);
-  tile.setAttribute("answer", questions[i].answer);
-  tile.setAttribute("points", questions[i].points);
-  tile.setAttribute("data-toggle", "modal");
-  tile.setAttribute("data-target", "#testModal");
-  document.body.appendChild(tile);
+  var tile= $('<tiles></tiles>')
+    .addClass('tiles')
+    .html(questions[i].points)
+    .attr("question", questions[i].question)
+    .attr("answer", questions[i].answer)
+    .attr("points", questions[i].points)
+    .attr("data-toggle", 'modal')
+    .attr("data-target", '#testModal');
+
+
+  
+  
+  $('body').append(tile);
+  // var tile = document.createElement('tiles');
+  // tile.className = 'tiles';
+  // tile.innerHTML = questions[i].points;
+  // tile.setAttribute("question", questions[i].question);
+  // tile.setAttribute("answer", questions[i].answer);
+  // tile.setAttribute("points", questions[i].points);
+  // tile.setAttribute("data-toggle", "modal");
+  // tile.setAttribute("data-target", "#testModal");
+  // document.body.appendChild(tile);
   
 }
 
@@ -64,33 +77,37 @@ function winLose(playerScore) {
 function updatePlayerScore() {
   $('#playerScore').text('Score:  ' + playerScore.toString());
 }
-$( "#target" ).submit(function() {
-  var pInput = $('#playerForm').val();
-
-  if(pInput === $('.tiles').attr("answer")){
-    console.log("true");
-  }
-  
-});
+// var $playerInput = $( "#target" ).submit(function() {
+//   var pInput = $('#playerForm').val(); 
+// });
 
 
+//When I click on tile, it opens up its modal with corresponding data
+$('.tiles').click(function(){
+  var playerInput = $('#playerForm').val();
+   $('.modal-body').html($('.tiles').attr("question"));  
+    $('#playerSubmt').val(playerInput);
 
+})
+//When I submit the form within the modal, it takes the value and places it in a variable
+// compare variable against answer of tile to check if right or wrong
 
-$('.tiles').on("click", function () {
-  
-  // $('.modal-header').text('Question');
-  $('.modal-body').html($('.tiles').attr("question"));
-  // var userInput = prompt(this.getAttribute("question"));
-  // var anyCase = userInput.toLowerCase();
-  // var points = this.getAttribute("points");
-  // if (anyCase === this.getAttribute("answer")) {
-  //   alert('correct');
-  //   playerScore += parseInt(points);
-  //   updatePlayerScore();
-  // } else {
-  //   alert('wrong');
-  //   playerScore -= this.getAttribute("points");
-  //   updatePlayerScore();
-  // }
-  // winLose(playerScore);
-});
+// $('.tiles').on("click", function () {
+//   var input = $playerInput;
+//   alert(input);
+//   // $('.modal-header').text('Question');
+//   $('.modal-body').html($('.tiles').attr("question"));
+//   // var userInput = prompt(this.getAttribute("question"));
+//   // var anyCase = userInput.toLowerCase();
+//   // var points = this.getAttribute("points");
+//   // if (anyCase === this.getAttribute("answer")) {
+//   //   alert('correct');
+//   //   playerScore += parseInt(points);
+//   //   updatePlayerScore();
+//   // } else {
+//   //   alert('wrong');
+//   //   playerScore -= this.getAttribute("points");
+//   //   updatePlayerScore();
+//   // }
+//   // winLose(playerScore);
+// });
