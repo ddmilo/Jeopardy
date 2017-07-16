b80710090696c2124ad6fc9986e0ba3dc4694c4b
ddmilo <ddmilo1029@gmail.com>
2017-07-15 20:31:38 -0400

    getting warmer I think on modal data grabs

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -2,31 +2,31 @@ var questions = [{
     question: 'What does HTML stand for?' ,
     answer: 'hyper text markup language',
     points: 100
-   }
-  // {
-  //   question: 'What is the capital of Georgia',
-  //   answer: 'atlanta',
-  //   points: 100
+   },
+  {
+    question: 'What is the capital of Georgia',
+    answer: 'atlanta',
+    points: 100
 
-  // },
-  // {
-  //   question: 'Which celebrity is known for their loud, colorful, one-liners?',
-  //   answer: 'samuel l. jackson',
-  //   points: 100
+  },
+  {
+    question: 'Which celebrity is known for their loud, colorful, one-liners?',
+    answer: 'samuel l. jackson',
+    points: 100
 
-  // },
-  // {
-  //   question: 'What is the baby\'s name on Family Guy? ',
-  //   answer: 'stewie',
-  //   points: 100
+  },
+  {
+    question: 'What is the baby\'s name on Family Guy? ',
+    answer: 'stewie',
+    points: 100
 
-  // },
-  // {
-  //   question: 'Why do this project?',
-  //   answer: 'boredom',
-  //   points: 100
+  },
+  {
+    question: 'Why do this project?',
+    answer: 'boredom',
+    points: 100
 
-  // }
+  }
 
 ];
 
@@ -44,20 +44,7 @@ for (i = 0; i < questions.length; i++) {
     .attr("data-toggle", 'modal')
     .attr("data-target", '#testModal');
 
-
-  
-  
   $('body').append(tile);
-  // var tile = document.createElement('tiles');
-  // tile.className = 'tiles';
-  // tile.innerHTML = questions[i].points;
-  // tile.setAttribute("question", questions[i].question);
-  // tile.setAttribute("answer", questions[i].answer);
-  // tile.setAttribute("points", questions[i].points);
-  // tile.setAttribute("data-toggle", "modal");
-  // tile.setAttribute("data-target", "#testModal");
-  // document.body.appendChild(tile);
-  
 }
 
 
@@ -84,13 +71,27 @@ function updatePlayerScore() {
 
 //When I click on tile, it opens up its modal with corresponding data
 $('.tiles').click(function(){
-  var playerInput = $('#playerForm').val();
-   $('.modal-body').html($('.tiles').attr("question"));  
-    $('#playerSubmt').val(playerInput);
+  $( "#target" ).submit(function(event) {
+  var event = $('#playerForm').val();
+    console.log($(this).attr("answer"));
+});
+   $('.modal-body').html($(this).attr("question"));
+   console.log($(this).val());
+})  
 
-})
+// compare $(this).attr("answer");
+  
+
+// function checkAnswer(){
+
+// }
+//On .submit check the answer by comparing .val of textfield with 
+
+
+
 //When I submit the form within the modal, it takes the value and places it in a variable
 // compare variable against answer of tile to check if right or wrong
+
 
 // $('.tiles').on("click", function () {
 //   var input = $playerInput;
